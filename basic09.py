418b18f3c417b42c606e2e844b52faa745c7aeca
cj3131 <callum2online@msn.com>
2016-11-12 00:54:12 +0000

    Renaming

--- basic09.py
+++ basic09.py
@@ -1,10 +1,10 @@
-def binarySearch(alist, item):
+    def binarySearch(alist, item, lowbound, upperbound):
     first = 0
     last = len(alist)-1
     found = False
 
-    while first<=last and not found:
-    	midpoint = (first + last)//2
+    while first <= last and not found:
+    	midpoint = (first + last) // 2
         if alist[midpoint] == item:
             found = True
         else:
@@ -15,6 +15,11 @@ def binarySearch(alist, item):
  
     return found
 
+
+low = int(input("Enter the lower boundary"))
+high = int(input("Enter the upper boundary"))
+
+
 testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
-print(binarySearch(testlist, 3))
-print(binarySearch(testlist, 13))
+print(binarySearch(testlist, 3, low, high))
+print(binarySearch(testlist, 13, low, high))
