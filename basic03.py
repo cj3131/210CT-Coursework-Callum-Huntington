f90807b2948f70c9476f89f5b091d62a428bfc3e
Callum Huntington <callum2online@msn.com>
2016-11-15 12:55:06 +0000

    Added comments
    Comments, improved variable names, general refactoring

--- basic03.py
+++ basic03.py
@@ -1,14 +1,23 @@
+""" First, we check if the given parameter is a square number. If it is not, we can check if the next biggest number is a square number.
+This continues until we reach a square number.
+
+Checking for a square is done by multiplying two identical integers together, beginning at 1*1 and incrementing from there. """"
+
 checknum = int(input("Enter the parameter, and the highest perfect square beneath it will be returned. "))
-x = checknum
-count = 1
+possibleSq = checknum
+counter = 1
 checkifsquare = True
 
 while checkifsquare == True:
-	if count * count > x:
-		count = 1
-		x -= 1
-	if count * count == x:
+	#Multiplication of the two integers exeeds the number we are checking, possibleSq, and so cannot be a square number.
+	#So decrement possibleSq to begin checking the next number
+	if counter * counter > possibleSq:
+		counter = 1
+		possibleSq -= 1
+	#Multiplication of two identical integers gives the number we are checking, so it is a square number
+	if counter * counter == possibleSq:
 		checkifsquare = False
-		print("The highest perfect square up to and including ",checknum, "is ", x)
-	if count * count < x:
-		count += 1
+		print("The highest perfect square up to and including ",checknum, "is ", possibleSq)
+	#Multiplication of the two identical integers gives a number smaller than the number we are checking, so we can increment them
+	if counter * counter < possibleSq:
+		counter += 1
