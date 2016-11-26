f62aa6835664be17c236edfe8bfbf3c0135d9155
Callum Huntington <callum2online@msn.com>
2016-11-26 18:34:03 +0000

    Added some pseudocode and description
    Needs addNode and addEdge pseudocode

--- basic13.py
+++ basic13.py
@@ -1,3 +1,28 @@
+"""
+Write the pseudocode for an unweighted graph data structure. You either use an adjacency
+matrix or an adjacency list approach. Also, write a function to add a new node and a
+function to add an edge. Following that, implement the graph you have designed in the
+programming language of your choice. You may use your own linked list from previous labs,
+the STL LL, or use a simple fixed size array (10 elements would be fine).
+
+Pseudocode:
+
+graph():
+    __init__():
+        nodes <-- []
+        matrix <-- [] * 5
+    addNode(value):
+        nodes.append(value)
+    addEdge(a,b):
+        matrix[a][b] <-- 1
+        matrix[b][a] <-- 1
+    display():
+        for i in range (0, length(matrix)):
+            print matrix[i]
+
+        
+"""
+
 class graph():
     def __init__(self):
         self.nodes = [] 
@@ -18,7 +43,7 @@ class graph():
         for i in range (0, len(self.matrix)):
             print(i, "     ", self.matrix[i])
     
-    def DFS(self):
+    def depthFirstSearch(self, g, node):
         pass
 
 g = graph()
