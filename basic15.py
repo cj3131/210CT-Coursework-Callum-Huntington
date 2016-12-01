3f7a5b3d3a20dfd5eb9718d96ff6c67475d36148
Callum Huntington <callum2online@msn.com>
2016-12-01 14:57:24 +0000

    Added testing
    Some issues with using tentative weight when value should be accessed

--- basic15.py
+++ basic15.py
@@ -35,7 +35,7 @@ class graph():
 
     def display(self):
         print("\n\nHere is the matrix with all of the edges added.\n")
-        print("         0  1  2  3  4  5  6  7  8  9")
+        print("         0    1    2     3    4    5   6    7    8    9")
         for i in range (0, len(self.matrix)):
             print(i, "     ", self.matrix[i])
     
@@ -45,7 +45,7 @@ class graph():
             node = self.searchStack.pop() # pop the node currently being viewed into node
             if node not in self.depthVisited: # if the node currently being viewed is not in the visited list
                 self.depthVisited.append(node) # add it to the visited list
-                print("visited via depth first search so far:  ", self.depthVisited, "\n")
+                print("visited via depth first search so far:  ", self.depthVisited)
                 
                 for idx, value in enumerate(self.matrix[node]):
                     if value != float("inf"):
@@ -60,7 +60,7 @@ class graph():
             node = self.searchQueue.pop(0)
             if node not in self.breadthVisited:
                 self.breadthVisited.append(node)
-                print("visited via breadth first search so far:  ", self.breadthVisited, "\n")
+                print("visited via breadth first search so far:  ", self.breadthVisited)
 
                 for idx, value in enumerate(self.matrix[node]):
                     if value != float("inf"):
@@ -72,15 +72,16 @@ class graph():
     def dijkstra(self, currentNode, destNode, nodeList):
         currentNode.tentativeWeight = 0
         visited = []
-        vertexValue = 0
         while currentNode != destNode:
             print("here")
             for idx, value in enumerate(self.matrix[currentNode.value]):
                 if value != float("inf"):
-                    vertexValue = value
-                    if currentNode.tentativeWeight + nodeList[idx].tentativeWeight < nodeList[idx].tentativeWeight:
+                    print(nodeList[idx].value)
+                    print(currentNode.tentativeWeight, nodeList[idx].tentativeWeight, nodeList[idx].value)
+                    if currentNode.tentativeWeight + nodeList[idx].tentativeWeight < nodeList[idx].value:
                         nodeList[idx].tentativeWeight = currentNode.tentativeWeight + nodeList[idx].tentativeWeight
                         nodeList[idx].previous = currentNode
+                        print(nodeList[idx], nodeList[idx].tentativeWeight)
             visited.append(currentNode)
             minimum = float("inf")
             for i in nodeList:
@@ -90,6 +91,8 @@ class graph():
                 elif i not in visited and i.tentativeWeight <= minimum:
                     currentNode = i
                     minimum = i.tentativeWeight
+
+        #prettyvisited = [ ]
 
         return visited
 
@@ -125,7 +128,12 @@ g.addEdge(nodeOne,nodeFive, 2)
 g.addEdge(nodeTwo,nodeThree, 1)
 g.addEdge(nodeSeven,nodeFour, 3)
 g.display()
-print("\n\n")
+print("\n")
 g.depthFirstSearch(1)
 g.breadthFirstSearch(1)
-print(g.dijkstra(nodeTwo,nodeFive,nodeList))
+x = g.dijkstra(nodeTwo,nodeFive,nodeList)
+
+for i in x:
+    print("Node ", i.value, "has tentative weight ", i.tentativeWeight)
+for i in x:
+    print("Path is ", i.previous)
